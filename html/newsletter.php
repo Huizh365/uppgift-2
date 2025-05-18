f5573d5a9db5ecd8ad71e67b1fb1089ade430561
hui zheng <huizh365@gmail.com>
2025-05-18 09:07:05 +0200

    newsletter page

--- html/newsletter.php
+++ html/newsletter.php
@@ -2,8 +2,28 @@
     require_once "includes/header.php";
 ?>
     <?php
-        echo ("newsletter detail")
-    ?>
+    
+    echo($_GET["id"]);
+
+    if(isset($_GET["id"])){
+        $id = $_GET["id"];
+
+        $database = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, "products");
+
+        $sql = "SELECT * FROM newsletters WHERE id = $id";
+        $result = $database->query($sql);
+
+        if($result->num_rows > 0) {
+            $newsletter = $result->fetch_assoc();
+            print_r($newsletter);
+        } else {
+            echo "No newsletter found";
+        }
+
+    } else {
+        echo "No id provided";
+    }
+?>
 <?php
     require_once "includes/footer.php"
 ?>
